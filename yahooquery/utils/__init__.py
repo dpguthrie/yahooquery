1770739087dd17b1c9ea53fd58a4d1f45753a2f9
dpguthrie <douglas.p.guthrie@gmail.com>
2019-12-13 18:31:46 -0700

    Remove unused functions

--- yahooquery/utils/__init__.py
+++ yahooquery/utils/__init__.py
@@ -1,9 +1,6 @@
 import requests
 import urllib3
 
-import pandas as pd
-import pandas.compat as compat
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -11,19 +8,3 @@ def _init_session(session):
     if session is None:
         session = requests.session()
     return session
-
-
-def _handle_lists(l, mult=True, err_msg=None):
-    if isinstance(l, (compat.string_types, int)):
-        return [l] if mult is True else l
-    elif isinstance(l, pd.DataFrame) and mult is True:
-        return list(l.index)
-    elif mult is True:
-        return list(l)
-    else:
-        raise ValueError(err_msg or "Only 1 symbol/market parameter allowed.")
-
-
-def no_pandas(out):
-    return out
-
