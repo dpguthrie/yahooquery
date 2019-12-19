84fb0cb469482c465492f69d45bbb8f184a81eab
dpguthrie <douglas.p.guthrie@gmail.com>
2019-12-19 16:47:13 -0700

    Create method to convert date to timestamp
    This method will be utilized in the Ticker class, specifically the history method.  It allows the user to specify a start and end date in either YYYY-MM-DD format or as a datetime object.  Additionally, it will supply default values in the event the period argument equals max (defaults to 1900-01-01 for start and now for end).

--- yahooquery/utils/__init__.py
+++ yahooquery/utils/__init__.py
@@ -1,10 +1,19 @@
 import requests
-import urllib3
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from datetime import datetime
+import time
 
 
 def _init_session(session):
     if session is None:
         session = requests.session()
     return session
+
+
+def _convert_to_timestamp(date=None, start=True):
+    if date is None:
+        date = int((-2208988800 * start) + (time.time() * (not start)))
+    elif isinstance(date, datetime):
+        date = int(time.mktime(date.timetuple()))
+    else:
+        date = int(time.mktime(time.strptime(str(date), '%Y-%m-%d')))
+    return date
