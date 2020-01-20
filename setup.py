c890eb98d56a321af678fd4b406877d8efee198f
dpguthrie <douglas.p.guthrie@gmail.com>
2020-01-20 15:49:33 -0700

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="yahooquery",
-    version="1.1.0",
+    version="1.1.1",
     author="Doug Guthrie",
     author_email="douglas.p.guthrie@gmail.com",
     description="Retrieve nearly all data from Yahoo Finance for one or more ticker symbols",
