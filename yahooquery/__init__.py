e095489885195b161cdb3c81778a8edf804ebf86
dpguthrie <douglas.p.guthrie@gmail.com>
2020-04-16 13:24:58 -0600

    Include Screener class

--- yahooquery/__init__.py
+++ yahooquery/__init__.py
@@ -2,5 +2,6 @@ name = 'yahooquery'
 __version__ = "2.0.0"
 
 from .ticker import Ticker  # noqa
+from .screener import Screener  # noqa
 from .misc import (  # noqa
     get_currencies, get_exchanges, get_market_summary, get_trending)
