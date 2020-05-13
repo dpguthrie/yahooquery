bd708dee4eaf6ee50702c8695bc227dfdc08b133
dpguthrie <douglas.p.guthrie@gmail.com>
2020-05-13 15:08:22 -0600

    Remove USER_AGENT_LIST from the Login class.
    Now I can utilize the same list in the init_session method to update the session's user-agent header

--- yahooquery/login.py
+++ yahooquery/login.py
@@ -7,21 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from yahooquery.utils import USER_AGENT_LIST
+
 
 class Login(object):
-
-    USER_AGENT_LIST = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-    ]
 
     LOGIN_URL = 'https://login.yahoo.com'
 
@@ -30,7 +19,7 @@ class Login(object):
         self.password = password
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument('--user-agent=' + kwargs.get(
-            'user_agent', random.choice(self.USER_AGENT_LIST)
+            'user_agent', random.choice(USER_AGENT_LIST)
         ))
         self.chrome_options.add_argument('headless')
         self.chrome_options.add_argument('--log-level=3')
