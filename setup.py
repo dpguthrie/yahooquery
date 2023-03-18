841f77e32e02b3e3332c8dcd23682a8d62ad9e7d
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-03-18 14:46:00 -0600

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="yahooquery",
-    version="2.3.0",
+    version="2.3.1",
     author="Doug Guthrie",
     author_email="douglas.p.guthrie@gmail.com",
     description="Retrieve nearly all data from Yahoo Finance for one or more ticker symbols",
