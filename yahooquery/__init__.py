41a93b745af9dde29382ffae9a5bf33664660826
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-07-15 13:26:06 -0600

    Update to 2.3.2

--- yahooquery/__init__.py
+++ yahooquery/__init__.py
@@ -1,7 +1,7 @@
 """Python interface to unofficial Yahoo Finance API endpoints"""
 
 name = "yahooquery"
-__version__ = "2.3.1"
+__version__ = "2.3.2"
 
 from .research import Research  # noqa
 from .ticker import Ticker  # noqa
